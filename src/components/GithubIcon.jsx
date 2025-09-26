355c23fb2e5dfa7cfd53b74bb967ae274799a154
Santiago Delgado Sanchez <santiagodelgadosanchez9@gmail.com>
2025-09-25 22:37:44 -0400

    fix size of icons

--- src/components/GithubIcon.jsx
+++ src/components/GithubIcon.jsx
@@ -1,20 +1,8 @@
 import * as React from "react";
-
-export const GithubIcon = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth={2}
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.2A4.2 4.2 0 0 0 17 9c.7-1.1 2-2 2-4a2.5 2.5 0 0 0-4-1.2 2.5 2.5 0 0 0-4 1.2 2.5 2.5 0 0 0-4-1.2 2.5 2.5 0 0 0-4 1.2A4.2 4.2 0 0 0 5 13c-.2.5-.5 1-.9 1.5A3.8 3.8 0 0 0 2 17v4h12c1.7 0 2.5-1.1 2.5-2.5V17a2.5 2.5 0 0 0-2.5-2.5h-1z" />
+const GithubIcon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.26c3.27-.35 6.74 1.63 6.74 5.86a.22.22 0 0 1-.22.22H12.5a.22.22 0 0 1-.22-.22c0-4.23 3.47-6.21 6.74-5.86a4.8 4.8 0 0 0-1-3.26v-4h-2.14a4.8 4.8 0 0 0-1-3.26" />
+    <path d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.16 6.84 9.46a.22.22 0 0 0 .22-.22v-1.78a.22.22 0 0 0-.22-.22C5.9 18.42 4.14 15.6 4.14 12c0-2.82 2.29-5.12 5.12-5.12 2.83 0 5.12 2.3 5.12 5.12a5.2 5.2 0 0 1-5.2 5.2c-2.83 0-5.12-2.3-5.12-5.12a.22.22 0 0 0-.22-.22" />
   </svg>
 );
-
-export default GithubIcon;
+export { GithubIcon };
